93200d04d6ce9d4186f9362ea440d2bb31873bdb
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-05-03 02:33:27 +0200

    root/root/autostart/apt-up.sh modified

--- root/root/autostart/apt-up.sh
+++ root/root/autostart/apt-up.sh
@@ -8,10 +8,14 @@ export LC_ALL=C.UTF-8 || export LC_ALL=C;
 export LINES=40 COLUMNS=132;
 export TERM=xterm-256color || export TERM=xterm || export TERM=linux || export TERM=vt100;
 
-while	stamp && read -r cmd && stamp "$cmd";
+while	stamp && read -ra cmd && stamp "$cmd";
 do
-	case "$cmd" in
-	('')	x /root/.apt && x /usr/bin/apt dist-upgrade && x /root/.apt;;
+	case "${#cmd[@]} ${cmd[0]}" in
+	('1 new')	exec "$0";;
+	([2-9]' ping')	printf -vT '%(%s)T' -2 && stamp PONG "$T" "${cmd[@]:1}";;
+	('1 up')	x /root/.apt && x /root/.apt;;
+	('1 dist')	x /root/.apt && x /usr/bin/apt dist-upgrade && x /root/.apt;;
+	(*)		stamp unknown command: "$cmd"; false; continue;;
 	esac;
 	stamp 'done';
 
